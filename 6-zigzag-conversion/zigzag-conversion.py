740949b799e6a9b38fdbb67b91e94b97f0016985
lok70 <64475032+lok70@users.noreply.github.com>
2025-01-26 23:12:08 +0300

    Time: 7 ms (89.76%) | Memory: 17.7 MB (67.91%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,31 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # Если зигзага нет, сразу возвращаем исходную строку
-        if numRows == 1 or numRows >= len(s):
+        if numRows == 1:
             return s
         
-        # Массив строк, каждая отвечает за один «ряд» зигзага
-        rows = [""] * numRows
+        rows = [''] * numRows
+        current_row = 0
+        direction = 1  # 1 для движения вниз, -1 для движения вверх
         
-        # Текущая позиция ряда и направление (идём вниз или вверх)
-        cur_row = 0
-        going_down = True
-        
-        # Раскладываем символы по «рядам»
         for char in s:
-            rows[cur_row] += char
-            # Если достигли нижней границы, меняем направление на «вверх»
-            if cur_row == numRows - 1:
-                going_down = False
-            # Если достигли верхней границы, меняем направление на «вниз»
-            elif cur_row == 0:
-                going_down = True
-            
-            # Движемся по рядам в зависимости от направления
-            if going_down:
-                cur_row += 1
-            else:
-                cur_row -= 1
+            rows[current_row] += char
+            # Определение направления движения
+            if current_row == 0:
+                direction = 1
+            elif current_row == numRows - 1:
+                direction = -1
+            current_row += direction
         
-        # Объединяем все ряды в один результат
-        return "".join(rows)
+        return ''.join(rows)
